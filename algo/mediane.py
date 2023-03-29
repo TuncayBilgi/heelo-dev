4234059807174661065b2c481df92c18ea038385
Tuncay Bilgi <tuncay.bilgi39@gmail.com>
2023-03-29 11:20:53 +0200

    Update mediane.py

--- algo/mediane.py
+++ algo/mediane.py
@@ -75,9 +75,8 @@ def sort_median(lst):
     lst.sort()
     return lst[len(lst)//2]
 
+
 # Exemple d'utilisation :
-
-
 
 
 
@@ -89,6 +88,73 @@ def sort_median(lst):
 
 lst = [random.randint(1,100) for x in range(50000)]
 
+
 print(moyenne(divideAndConquer,lst))
 print(moyenne(quick_sort_median,lst))
 print(moyenne(sort_median,lst))
+
+
+
+# Python3 program of Quick Select
+  
+# Standard partition process of QuickSort(). 
+# It considers the last element as pivot 
+# and moves all smaller element to left of 
+# it and greater elements to right
+def partition(arr, l, r):
+      
+    x = arr[r]
+    i = l
+    for j in range(l, r):
+          
+        if arr[j] <= x:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+              
+    arr[i], arr[r] = arr[r], arr[i]
+    return i
+  
+# finds the kth position (of the sorted array) 
+# in a given unsorted array i.e this function 
+# can be used to find both kth largest and 
+# kth smallest element in the array. 
+# ASSUMPTION: all elements in arr[] are distinct
+def kthSmallest(arr, l, r, k):
+  
+    # if k is smaller than number of
+    # elements in array
+    if (k > 0 and k <= r - l + 1):
+  
+        # Partition the array around last
+        # element and get position of pivot
+        # element in sorted array
+        index = partition(arr, l, r)
+  
+        # if position is same as k
+        if (index - l == k - 1):
+            return arr[index]
+  
+        # If position is more, recur 
+        # for left subarray 
+        if (index - l > k - 1):
+            return kthSmallest(arr, l, index - 1, k)
+  
+        # Else recur for right subarray 
+        return kthSmallest(arr, index + 1, r, 
+                            k - index + l - 1)
+    print("Index out of bound")
+  
+# Driver Code
+meantime = 0
+for i in range(10) :
+    
+    lst = [random.randint(1,100) for x in range(50000)]
+    n = len(lst)
+    k = len(lst)//2
+    start= time.time()
+    kthSmallest(lst, 0, n - 1, k)
+    end= time.time()
+    elapsed = end-start
+    meantime += elapsed
+print(f"La fonction Quickselect prend en moyenne {meantime} ms pour n={len(lst)}")
+  
